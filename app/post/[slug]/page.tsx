579e949589901e774b22ab65ef95452e0a0ab4e8
Stokori <stokoriel@gmail.com>
2024-06-01 14:49:43 -0500

    initial commit

---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+
+export async function generateStaticParams() {
+    // get all your mdx files
+    const filenames: string[] = fs.readdirSync(path.join('./posts'));
+
+    // let's use the filenames without `.mdx` extension as our slugs
+    const posts = filenames.map((filename) => {
+        return {
+            slug: filename.replace(/\.mdx$/, ""),
+        };
+    });
+
+    return posts.map((post) => ({
+        slug: post.slug,
+    }))
+}
+
+function getPost({slug} : {slug : string}){
+    const markdownFile = fs.readFileSync(path.join('posts',slug + '.mdx'), 'utf-8')
+
+    const { data: frontMatter, content } = matter(markdownFile)
+
+    return {
+        frontMatter,
+        slug,
+        content
+    }
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+
+    const props = getPost(params);
+
+    return (
+        <article className='prose prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert mx-auto'>
+            <h2>{props.frontMatter.title}</h2>
+            <MDXRemote source={props.content}/>
+        </article>
+    )
+}
---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return ( <div className="px-4 md:px-0 mx-auto bg-slate-800 py-5 px-5 border-t-2 border-slate-200">
+    <div className="mx-0 sm:mx-6 text-center">
+    © 2024 perappu
+    </div>
+  </div>)
+  }
