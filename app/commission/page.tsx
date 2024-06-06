adf65ef9bc6fae115b087501673cda82a2fd148e
Stokori <stokoriel@gmail.com>
2024-06-06 16:28:27 -0500

    fix it again

--- app/commission/page.tsx
+++ app/commission/page.tsx
@@ -1,18 +1,18 @@
 export default function LorekeeperCommissions() {
     return (<>
-        <article className='prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-4'>
+        <article className="prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-4">
             <h2>Lorekeeper Commissions</h2>
         </article>
-        <article className='prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5'>
-            Thank you for considering me for your developer work! ❤️ I'd love to work with you. Read below for more information!
+        <article className="prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5">
+            Thank you for considering me for your developer work! ❤️ I&apos;d love to work with you. Read below for more information!
         </article>
         <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <article className='prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5'>
+        <article className="prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5">
             <h3><a href="#offer">What I Offer</a></h3>
-            <p className='mx-5'>For people who prefer a more &quot;hands-off approach&quot; to Lorekeeper, I provide <strong>managed hosting</strong>. What does this mean? I can do <i>nearly everything</i> coding-wise for your site.</p>
+            <p className="mx-5">For people who prefer a more &quot;hands-off approach&quot; to Lorekeeper, I provide <strong>managed hosting</strong>. What does this mean? I can do <i>nearly everything</i> coding-wise for your site.</p>
 
-            <p className='mx-5'>I can:</p>
-                <ul className='mx-5'>
+            <p className="mx-5">I can:</p>
+                <ul className="mx-5">
                     <li>Manage all aspects of your hosting set up -- domain name, files, images, database, sendgrid...</li>
                     <li>Keep your server secured</li>
                     <li>Keep your site updated to the newest version of LK v3.</li>
@@ -23,40 +23,40 @@ export default function LorekeeperCommissions() {
                     <li>...and more depending on your needs!</li>
                 </ul>
         </article>
-        <article className='prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5'>
+        <article className="prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5">
             <h3><a href="#newsite">New Sites</a></h3>
-            <p className='mx-5'>Initial dev work is <strong>$40/hour</strong>, which will be tracked via message timestamps. <strong>Initial site setup is a minimum of $40.</strong></p>
+            <p className="mx-5">Initial dev work is <strong>$40/hour</strong>, which will be tracked via message timestamps. <strong>Initial site setup is a minimum of $40.</strong></p>
 
-            <p className='mx-5'>I try to be extremely fair with what counts as billable time. <strong>Time tracking is done via Discord message timestamps.</strong> I will send you messages periodically throughout the development process, so you are aware exactly when and what I&apos;m working on!</p>
+            <p className="mx-5">I try to be extremely fair with what counts as billable time. <strong>Time tracking is done via Discord message timestamps.</strong> I will send you messages periodically throughout the development process, so you are aware exactly when and what I&apos;m working on!</p>
 
-            <p className='mx-5'><strong>Hosting starts at $20/month.</strong> This gets you the most basic plan offered by DigitalOcean:</p>
+            <p className="mx-5"><strong>Hosting starts at $20/month.</strong> This gets you the most basic plan offered by DigitalOcean:</p>
 
-            <p className='mx-5'>If you wish for a faster server, the cost per month will go up accordingly. I&apos;m flexible with payment methods and you&apos;re free to pay for several months in advance.</p>
+            <p className="mx-5">If you wish for a faster server, the cost per month will go up accordingly. I&apos;m flexible with payment methods and you&apos;re free to pay for several months in advance.</p>
 
-            <p className='mx-5'><strong>This is a better deal than DreamHost!</strong> Why? Well...</p>
-            <p className='mx-5'>The $20 per month includes:</p>
-                <ul className='mx-5'>
+            <p className="mx-5"><strong>This is a better deal than DreamHost!</strong> Why? Well...</p>
+            <p className="mx-5">The $20 per month includes:</p>
+                <ul className="mx-5">
                     <li>Site and image hosting</li>
                     <li>Updates to LK</li>
                     <li>Updates to plugins</li>
                     <li>Basic tech support, bug fixes (&quot;It suddenly broke and I don&apos;t know how to fix it&quot;)</li>
                 </ul>
 
-            <p className='mx-5'><strong>Additional dev work after initial site setup is discounted for people I host at $35/hour.</strong> This is as a thank you for choosing me for your hosting!</p>
+            <p className="mx-5"><strong>Additional dev work after initial site setup is discounted for people I host at $35/hour.</strong> This is as a thank you for choosing me for your hosting!</p>
 
             <h3><a href="#existing">Existing Sites</a></h3>
-            <p className='mx-5'>If you have an existing LK site and would like to transfer to my services, I&apos;m happy to help! I charge $40/hour with a one hour minimum charge. You will need to be comfortable providing any necessary logins. After that, you benefit from the discounted $35/hour rate.</p>
+            <p className="mx-5">If you have an existing LK site and would like to transfer to my services, I&apos;m happy to help! I charge $40/hour with a one hour minimum charge. You will need to be comfortable providing any necessary logins. After that, you benefit from the discounted $35/hour rate.</p>
 
             <h3><a href="#generalwork">General Work</a></h3>
-            <p className='mx-5'>I can also help with general dev work at my standard $40/hour charge. This can include adding plugins, updating your site, etc. For the time being, I don&apos;t offer creation of new plugins.</p>
+            <p className="mx-5">I can also help with general dev work at my standard $40/hour charge. This can include adding plugins, updating your site, etc. For the time being, I don&apos;t offer creation of new plugins.</p>
 
         </article >
         <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <article className='prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5'>
+        <article className="prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5">
             <h3><a href="#tos">Terms of Service</a></h3>
             <p><strong>By purchasing my services you automatically agree to the following terms and conditions. It is your responsibility to be familiar with these terms. They are subject to change at any time.</strong></p>
             <h4>General</h4>
-            <ul className='mx-5'>
+            <ul className="mx-5">
                 <li>I reserve the right to deny service to anyone without explanation.</li>
                 <li>I reserve the right to use commissioned work for my portfolio, unless requested otherwise.</li>
                 <li>I will not use any PII (personal identifying information) for any use other than as required to provide the services requested.</li>
@@ -64,17 +64,17 @@ export default function LorekeeperCommissions() {
                 <li>The right to use my work is not an endorsement in any way.</li>
             </ul>
             <h4>Payment</h4>
-            <ul className='mx-5'>
+            <ul className="mx-5">
                 <li>The initial $40 is paid upfront. An invoice for hourly charges will be delivered after the work is completed. The site will be turned on and code delivered after that invoice has been paid.</li>
                 <li>Hosting payment has a 15 day grace period. If you do not pay before the 15th of the month, your site will be turned off until paid.</li>
                 <li>You may pay for multiple months in advance. However, I do not offer discounts at this time.</li>
                 <li>Payment may be done via Paypal or Stripe. If neither is available in your country, I&apos;m open to other options!</li>
             </ul>
             <h4>Hosting Details</h4>
-            <ul className='mx-5'>
+            <ul className="mx-5">
                 <li>I can work with other hosting options (such as DreamHost), but I can not provide support past initial set up.</li>
                 <li>Domain names <i>for people I host</i> will be purchased via Namecheap and managed on my own account for ease. It&apos;s also a separate purchase from what I charge for dev work -- expect to pay about $20/year. There&apos;s a legally required 60 day period before we can transfer the domain, but after that timeframe you can manage it yourself if you would prefer. If I am not hosting your site, you&apos;re responsible for your own domain.</li>
-                <li>Your website's code will be available via a private GitHub repository at any time.</li>
+                <li>Your website&apos;s code will be available via a private GitHub repository at any time.</li>
                 <li>If you have another coder for your site, I have no problem working with them! I don&apos;t offer any discounts for that, however.</li>
                 <li>I typically do not provide email accounts, but this can be discussed depending on your needs. I&apos;m happy to help you set something up.</li>
                 <li>If you wish to shut down, I will store your database for six months. We can discuss your options if you need it stored longer. Your code will be available on GitHub indefinitely, unless you request otherwise. Data can be deleted at your request at any time, in compliance with EU law.</li>
@@ -82,7 +82,7 @@ export default function LorekeeperCommissions() {
                 <li>If you decide to move off of my hosting services, I have no problem helping you migrate. I do not offer unpaid support after you migrate to a different host. However, you may hire me for additional dev services at my standard rate.</li>
             </ul>
             <h4>Expectations of Me & You</h4>
-            <ul className='mx-5'>
+            <ul className="mx-5">
                 <li>I will provide a fair and reasonable timeline for when your work will be completed, and I will provide an advance warning if the expected timeframe changes.</li>
                 <li>I check my work messages between 4 and 5 PM CST on weekdays, and sporadically on Saturday (typically early in the morning). I don&apos;t work on Sundays. I&apos;ll try to get back to you as soon as possible, but I work 40 hour full-time day job so responses may be delayed. Expect me to get back to you within 1-2 days, excluding vacations or other time off. Please be reasonable -- this is my side job, and I support a family.</li>
                 <li>Due to my schedule, &quot;working&quot; hour availability (the billable time that I am actively working on your site) may be sporadic. You don&apos;t need to be available exactly at the same time I&apos;m working -- however, you must be comfortable with me messaging you status updates regardless of whether or not you are available at that very moment.</li>
