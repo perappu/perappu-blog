9ad4c12ef565f3ec7bcb864f8a1acf954f39c8ed
Stokori <stokoriel@gmail.com>
2024-06-22 13:00:04 -0500

    maybe this will make github actions happy

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const withMDX = require("@next/mdx")()
-
-module.exports = (phase, { defaultConfig }) => {
-  return withMDX({
-    reactStrictMode: true,
-    pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
-  })
-}
