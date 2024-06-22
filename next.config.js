d896b3dd2442b5ac90b98381972a661c12aa781b
Stokori <stokoriel@gmail.com>
2024-06-22 12:52:43 -0500

    github pages explode

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const withMDX = require('@next/mdx')()
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+const withMDX = require("@next/mdx")()
+
+module.exports = (phase, { defaultConfig }) => {
+  return withMDX({
+    reactStrictMode: true,
+    pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
+  })
 }
- 
-module.exports = withMDX(nextConfig)
