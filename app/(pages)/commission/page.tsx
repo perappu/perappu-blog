47577d2728127bb4daf6746e521df57d584675ed
Kazimir <stokoriel@gmail.com>
2024-08-16 08:44:07 -0500

    add art commissions

--- app/(pages)/commission/page.tsx
+++ app/(pages)/commission/page.tsx
@@ -10,6 +10,11 @@ export default function Commissions() {
                     <li><a href="/commission/lorekeeper">Lorekeeper Commissions</a></li>
                     <li>Toyhouse Commissions (coming soon)</li>
                 </ul>
-        
+        <article className="prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-4">
+            <h2>Art Commissions</h2>
+        </article>
+        <article className="prose !max-w-none prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert !mt-5">
+            <p className="mx-5">On occasion I also offer art commissions. See my price sheet and TOS <a href="/commission/art/">here</a>.</p>
+        </article>
             </>)
 }
